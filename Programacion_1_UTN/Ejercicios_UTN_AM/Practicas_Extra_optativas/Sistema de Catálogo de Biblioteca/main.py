b5038984a89a674547f5a1716d47f94b7a48ced6
UlisesPariente <ulisespariente1@gmail.com>
2025-09-15 12:57:59 -0300

    Update main.py

--- Programacion_1_UTN/Ejercicios_UTN_AM/Practicas_Extra_optativas/Sistema de Catálogo de Biblioteca/main.py
+++ Programacion_1_UTN/Ejercicios_UTN_AM/Practicas_Extra_optativas/Sistema de Catálogo de Biblioteca/main.py
@@ -1,52 +1,25 @@
 import Constantes
-#3. Opciones del menú:
-#1. Cargar títulos y ejemplares
-#Permitir al usuario ingresar hasta 20 títulos y la cantidad de ejemplares para cada uno.
 
-while flag_exit:
-    
-    for i in range (Constantes.CANTIDAD_DE_LIBROS):
-        print (f"\n////Titulo////")
-        Constantes.TITULOS [i] = Constantes.carga_de_datos()
-        print (f"\n////Ejemplares////")
-        Constantes.EJEMPLARES [i] = Constantes.carga_de_datos()
-    
-    
-        while True:
-            exit = str(input("\n-Desea continuar? (Y/N) "))
-            if exit == "n" or "N":
-                flag_exit = False
-                print (f"\nFinalizando el proceso.")
-                break
-            elif exit == "Y" or "y":
-                print (f"\nPerfecto continuemos")
-            else:
-                print (f"\nDisculpa no entendi.")
-        if flag_exit == False:
+
+while Constantes.flag_exit:
+    menu = int(input(f"Bienvenido a la biblioteca\nSeleccione una de las opciones:\n[1] Cargar titulos y ejemplares\n[2] Mostrar catalogo completo\n[3] Consultar disponibilidad\n[4] Listar títulos agotados\n[5] Agregar un nuevo título\n[6] Actualizar ejemplares\n[7] Salir"))
+    match menu: 
+        case 1:
+            Constantes.Cargar_titulos_y_ejemplares()
+        case 2:
+            Constantes.Mostrar_catalogo_completo()
+        case 3:
+            Constantes.Consultar_disponibilidad()
+        case 4:
+            Constantes.listar_titulos_agotados()
+        case 5:
+            Constantes.agregar_un_nuevo_titulo()
+        case 6:
+            Constantes.actualizar_ejemplares()
+        case 7:
+            print (f"/////////////////\nFinalizando el programa\n/////////////////")
             break
-        
-#2. Mostrar catálogo completo
-#Listar cada título con su número de ejemplares.
-#Ejemplo: El Señor de los Anillos → 5 copias
-for i in range (Constantes.CANTIDAD_DE_LIBROS):
-    print (f"\n{i+1}_ {Constantes.TITULOS[i]} -> {Constantes.EJEMPLARES[i]} copias")
-#3. Consultar disponibilidad
-#Pedir al usuario un título y mostrar cuántas copias tiene.
-titulo_a_buscar = str(input("Ingrese el titulo a buscar: "))
-for i in range (len(Constantes.TITULOS)):
-    if titulo_a_buscar == Constantes.TITULOS[i]:
-        print (f"Se encontro el titulo {titulo_a_buscar} -> {Constantes.EJEMPLARES} copias")
-#4. Listar títulos agotados
-#Mostrar solo aquellos títulos que tengan 0 copias.
-for i in range (len(Constantes.TITULOS)):
-    if Constantes.EJEMPLARES[i] == 0:
-        print(f"\n{Constantes.TITULOS} AGOTADO")
-#5. Agregar un nuevo título
-#Permitir al usuario agregar un nuevo libro y su cantidad de ejemplares si no se superó el máximo
-#de 20.
-
-#6. Actualizar ejemplares (préstamo / devolución)
-#Permitir al usuario modificar el número de ejemplares de un libro existente.
+    
 #7. Salir
 #Ejemplo de listas paralelas:
 #titulos = ["El Señor de los Anillos", "Orgullo y Prejuicio", "Matar un Ruiseñor"]
@@ -56,3 +29,7 @@ for i in range (len(Constantes.TITULOS)):
 #● Manejar correctamente los índices para mantener sincronizados títulos y ejemplares.
 #● Evitar sobrepasar el límite de 20 elementos.
 #● Modularizar usando funciones para cada opción del menú.
+    
+    
+       
+ 
