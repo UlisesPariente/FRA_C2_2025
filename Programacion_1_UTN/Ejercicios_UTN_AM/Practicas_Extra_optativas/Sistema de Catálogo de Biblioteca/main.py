d865bbf8b9edf8cf381d8fa6f20db0c289e6bcb8
UlisesPariente <ulisespariente1@gmail.com>
2025-09-15 11:33:52 -0300

    Update main.py

--- Programacion_1_UTN/Ejercicios_UTN_AM/Practicas_Extra_optativas/Sistema de Catálogo de Biblioteca/main.py
+++ Programacion_1_UTN/Ejercicios_UTN_AM/Practicas_Extra_optativas/Sistema de Catálogo de Biblioteca/main.py
@@ -5,23 +5,44 @@ import Constantes
 
 while flag_exit:
     
-    for i in range (20):
-        EJEMPLARES [carga_de_datos()]
+    for i in range (Constantes.CANTIDAD_DE_LIBROS):
+        print (f"\n////Titulo////")
+        Constantes.TITULOS [i] = Constantes.carga_de_datos()
+        print (f"\n////Ejemplares////")
+        Constantes.EJEMPLARES [i] = Constantes.carga_de_datos()
     
     
-        exit = str(input("Desea continuar? (Y/N) "))
-    if exit == "n" or "N":
-        flag_exit = False
-        print (f"Finalizando el proceso.")
-
+        while True:
+            exit = str(input("\n-Desea continuar? (Y/N) "))
+            if exit == "n" or "N":
+                flag_exit = False
+                print (f"\nFinalizando el proceso.")
+                break
+            elif exit == "Y" or "y":
+                print (f"\nPerfecto continuemos")
+            else:
+                print (f"\nDisculpa no entendi.")
+        if flag_exit == False:
+            break
+        
 #2. Mostrar catálogo completo
 #Listar cada título con su número de ejemplares.
 #Ejemplo: El Señor de los Anillos → 5 copias
+for i in range (Constantes.CANTIDAD_DE_LIBROS):
+    print (f"\n{i+1}_ {Constantes.TITULOS[i]} -> {Constantes.EJEMPLARES[i]} copias")
 #3. Consultar disponibilidad
 #Pedir al usuario un título y mostrar cuántas copias tiene.
+titulo_a_buscar = str(input("Ingrese el titulo a buscar: "))
+for i in range (len(Constantes.TITULOS)):
+    if titulo_a_buscar == Constantes.TITULOS[i]:
+        print (f"Se encontro el titulo {titulo_a_buscar} -> {Constantes.EJEMPLARES} copias")
 #4. Listar títulos agotados
 #Mostrar solo aquellos títulos que tengan 0 copias.
+for i in range (len(Constantes.TITULOS)):
+    if Constantes.EJEMPLARES[i] == 0:
+        print(f"\n{Constantes.TITULOS} AGOTADO")
 #5. Agregar un nuevo título
+
 #Permitir al usuario agregar un nuevo libro y su cantidad de ejemplares si no se superó el máximo
 #de 20.
 #6. Actualizar ejemplares (préstamo / devolución)
