443058ddc341fe14813eb5defdb910c7fa45df2f
UlisesPariente <ulisespariente1@gmail.com>
2025-09-20 17:12:17 -0300

    Update Vectores.py

--- Programacion_1_UTN/Ejercicios_UTN_AM/Vectores.py
+++ Programacion_1_UTN/Ejercicios_UTN_AM/Vectores.py
@@ -49,26 +49,68 @@ print (f"Cantidad de numeros mayores a 100, ingresados es de {contador}")
 #5. Buscar un valor:
 #Cargar un array de 10 enteros. Solicitar al usuario un número y verificar si se encuentra en el array.
 #Informar la posición en caso afirmativo, o indicar que no se encuentra.
+FLAG = False
 
-array_eje5 = [0]*10°
+array_eje5 = [0]*10
 for i in range (len(array_eje5)):
     if num == array_eje5[i]:
-        print (f"se enconcontr en la posicion {i} del array")
-    else:
-        print (f"No se encuentra")
-        
+        donde = array_eje5 [i]
+        FLAG=True
+        break
+if FLAG == True:
+    print (f"se enconcontr en la posicion {donde} del array")   
+else:
+    print (f"No se encuentra")
            
-#Cargar un array de 7 números enteros. D
-#6. Mayor y su posición:eterminar el valor más alto y en qué posición se
+#Mayor y su posición:
+#Cargar un array de 7 números enteros. Determinar el valor más alto y en qué posición se
 #encuentra.
+array_eje6 =[0]*7
+
+for i in range (len (array_eje6)):
+    num = int (input("Cargar dato: "))
+    array_eje6 [i] = num
+    if i == 0:
+        num_max = num
+    elif num > num_max:
+        num_max =num 
+        donde = i+1
+print (f"Numero mas alto: {num_max}\nPosicion: {donde}")           
 #7. Invertir orden:
 #Cargar un array de 6 enteros y mostrarlo invertido, es decir, desde el último al primero.
+array_7 = [0]*6
+for i in range (len (array_7)):
+    num = int(input("Cargar datos: "))
+    array_7 [i]= num
+for i in range (len(array_7),-1,-1):
+    print (f"{array_7[i]}") 
 #8. Comparar dos arrays:
 #Cargar dos arrays de 5 elementos cada uno. Comparar si ambos son iguales elemento a elemento
 #y mostrar un mensaje indicando si son o no iguales.
+array_8A = [0]*5
+array_8B = [0]*5
+for i in range (len(array_8A)): 
+    num=int (input("Cargar datos para primer array: "))
+    array_8A [i]=num
+for i in range (len(array_8B)): 
+    num=int (input("Cargar datos para primer array: "))
+    array_8B [i]=num
+contador = 0
+for i in range (5):
+    if array_8A [i] == array_8B[i]:
+        contador +=1
+if contador == 5:
+    print (f"Comparten los mismos elementos")
 #9. Intercambiar elementos pares por ceros:
 #Cargar un array de 10 enteros. Reemplazar todos los elementos pares por cero y mostrar el array
 #resultante.
+array_9 = [0]*10
+for i in range (len(array_9 )):
+    num = int (input("Cargar datos: "))
+    if num % 2 == 0:
+        array_9[i]= 0
+    array_9[i]=num
+print (f"{array_9}")
 #10. Función para buscar la primera aparición de un valor:
 #Escribir una función que reciba un array de enteros y un número a buscar. La función debe retornar
 #la posición de la primera aparición de ese número o -1 si no se encuentra.
