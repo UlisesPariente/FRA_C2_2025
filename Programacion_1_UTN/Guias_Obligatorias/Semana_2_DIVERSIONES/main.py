ba7a3e948276bab1976cb7971e43b909a2df4558
UlisesPariente <ulisespariente1@gmail.com>
2025-10-26 00:24:20 -0300

    Create main.py

---
+++ Programacion_1_UTN/Guias_Obligatorias/Semana_2_DIVERSIONES/main.py
@@ -0,0 +1,6 @@
+import paquetes
+
+print(f"══════════════════PYTHONLAND════════════════════════\nBienvenido a nuestro parque de diversiones.\n════════════════════════════════════════════════")
+    
+paquetes.mostrar_atracciones()
+
