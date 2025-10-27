968d922c7c50e25231cf3a90dac6a1f6326dc8a9
UlisesPariente <ulisespariente1@gmail.com>
2025-10-26 23:26:46 -0300

    Create Archivo_juegos.py

---
+++ Programacion_1_UTN/Semana_a_Semana/Semana_10/Datos/Archivo_juegos.py
@@ -0,0 +1,18 @@
+#crear toda la logica del archivo
+import os
+def cargar_catalogo (nombre_archivo):
+    catalogo = []
+#utiliza la funcion OS para verificar que exista, esto logicamente si exitste va a ser un tru y si no existe va a ser un false
+    if not os.path.exists(nombre_archivo):
+        #si el archivo no existe retorna la lista vacia.
+        print ("El archivo no existe aun... ")
+        return catalogo
+
+#abrir archivo en modo "r" (lectura)
+
+    with open(nombre_archivo, "r") as archivo:
+        encabezado = archivo.readline () # se lee primer linea (nombre de columna y descarta
+        
+        for line in archivo:
+            line.strip()# primero elimina las posibilidades de los espacios en vacio al principio o final del campo
+            if 
