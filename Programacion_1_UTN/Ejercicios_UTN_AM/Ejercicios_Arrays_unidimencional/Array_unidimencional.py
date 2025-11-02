0b83ac293f4a2b28d4c9609a3f8e9f5ab3b93065
UlisesPariente <ulisespariente1@gmail.com>
2025-11-02 02:47:45 -0300

    Update Array_unidimencional.py

---
+++ Programacion_1_UTN/Ejercicios_UTN_AM/Ejercicios_Arrays_unidimencional/Array_unidimencional.py
@@ -0,0 +1,42 @@
+#Arrays unidimensionales
+#
+#Desarrollar una función que permita crear un array de números con la cantidad de elementos que establezca el parámetro recibido.
+
+def array_numeros_establecida ():
+    lista = []
+    while True: 
+        Num = int(input("Ingrese un numero, para finalizar ingrese '*': "))
+        lista.append (Num)
+        if Num == "*":
+            print("Finalizando...")    
+            return lista
+
+#Escribir una función que permita ingresar la cantidad de números que reciba como parámetro.  Crear el array con la función del punto 1.
+
+
+
+#Escribir una función que reciba una lista de enteros, la misma calculará y devolverá el promedio de todos los números. 
+
+
+
+#Escribir una función parecida a la anterior, pero la misma deberá calcular y devolver el promedio de los números positivos.
+#Escribir una función que calcule y retorne el producto de todos los elementos de la lista que recibe como parámetro.
+#Escribir una función que reciba como parámetros una lista de enteros y retorne la posición del valor máximo encontrado.
+#Escribir una función que reciba como parámetros una lista de enteros y muestre la/las posiciones en donde se encuentra el valor máximo hallado.
+#Implementar una función llamada reemplazar_nombres que reciba los siguientes parámetros:
+#Una lista de nombres (lista_nombres).
+#Un nombre a buscar en la lista (nombre_antiguo).
+#Un nombre de reemplazo (nombre_nuevo).
+#La función debe realizar las siguientes acciones:
+#Reemplazar todas las apariciones de nombre_antiguo en lista_nombres por nombre_nuevo.
+#Retornar la cantidad total de reemplazos realizados.
+#
+#Crear una función que reciba como parámetros dos arrays. La función deberá mostrar la intersección de los dos arrays.
+#
+#
+#
+#
+#Crear una función que reciba como parámetros dos arrays. La función deberá mostrar la unión de los dos arrays.
+#
+#Crear una función que reciba como parámetros dos arrays. La función deberá mostrar la diferencia de los dos arrays.
+#
